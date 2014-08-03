599c885e04a99b43d27046b156bf362411021ff6
Ryuichi Yamamoto <zryuichi@gmail.com>
2014-08-03 21:38:10 +0900

    Add swipe example

--- python/example.py
+++ python/example.py
@@ -1,26 +1,35 @@
 import sptk
 import numpy as np
 from pylab import *
+import scipy.io.wavfile
 
 print "peform gexp"
 print sptk.gexp(1.0, 2.0)
 
-x = np.arange(256, dtype=float)
-y = np.arange(256, dtype=float)
+fs, data = scipy.io.wavfile.read("test.wav")
+print data.shape
+data = np.array(data, dtype=float)
 
-#print sptk.mcep(x, 512, y, 20, 0.41, 1, 10, 0.001, 0, 0.0001, 0.0001, 0)
-xx = np.random.randn(256) + 10.0
+x = data[2000:2000+256]
+y = data[2000:2000+256]
+
+xx = x
 print xx.shape
 ret, mc = sptk.mcep(xx, 30, 0.41, 2, 30, 0.001, 0, 0.0, 0.00001, 0)
 if True:
-    print mc
-    print np.array(mc).shape
-    plot(mc)
+    plot(mc, label="mcep")
+    legend()
     show()
     quit()
 
 print sptk.fft(x, y)
 spec = sqrt(x*x+y*y)
+
+f0 = np.zeros(data.shape[0]/80+1, dtype=float64)
+sptk.swipe(data, f0, fs, 80, 20.0, 800.0, 0.3, 1)
+
+plot(f0)
+show()
 
 plot(spec)
 show()
